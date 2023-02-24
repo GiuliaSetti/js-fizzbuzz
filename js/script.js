36529bb6007406b4a5eed8c9b211e80480952093
GiuliaSetti <giulia.setti96@gmail.com>
2023-02-24 16:00:00 +0100

    scaffolding and pseudocode

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Consegna:
+// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// BONUS 1: Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+// BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+
+
+/* Creo una variabile let nomeEl = document.getByElementId (id elemento).
+
+
+- creo un ciclo con variabile index minore di 100.
+
+- creo un elemento di tag div
+
+- con .append() lo attribuisco come figlio di div principale
+
+?SE i è divisibile i % 3 == 0 e i % 5 == 0 la console da FizzBuzz;
+
+    :ALTRIMENTI SE i è divisibile i % 3 == 0 la console da Fizz;
+
+    :ALTRIMENTI SE i è divisibile i % 5 == 0 la console da Buzz;
+
+    :ALTRIMENTI in console si mostrerà i.
+
+*/
+
